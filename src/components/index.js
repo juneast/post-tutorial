949e2569d8c491cb2ffd631425f5e94a192f13d7
LIMDONGJUN <dlaehd1994@naver.com>
2020-05-01 10:12:36 +0900

    중간과정

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./Header/Header";
+import PostWrapper from "./PostWrapper/PostWrapper"
+import Navigate from "./Navigator/Navigator"
+export {
+    Header,
+    PostWrapper,
+    Navigate
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import 'semantic-ui-css/semantic.min.css'
+import App from './App';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-serviceWorker.unregister();
